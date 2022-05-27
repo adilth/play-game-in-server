7a79afe2182310033d2145b67793b90d8ea6c80e
adil tahiri <rajaadil1995@gmail.com>
2022-05-27 19:48:38 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,102 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const querystring = require("querystring");
+const figlet = require("figlet");
+const express = require("express");
+const app = express();
+
+const server = http.createServer((req, res) => {
+  function createfile(filename, contentType) {
+    fs.readFile(filename, (err, data) => {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.write(data);
+      res.end();
+    });
+  }
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  switch (page) {
+    case "/":
+      createfile("index.html", "text/html");
+      break;
+    case "/otherpage.html":
+      createfile("otherpage.html", "text/html");
+      break;
+    case "/otherotherpage":
+      createfile("otherotherpage.html", "text/html");
+      break;
+    case "/tic-tac-toe/index.html":
+      createfile("tic-tac-toe/index.html", "text/html");
+      break;
+    case "/api":
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      if ("student" in params) {
+        let adjArray = [
+          "colorful",
+          "happy",
+          "enthusiastic",
+          "agreeable",
+          "funky",
+        ];
+        let verbArray = ["dancing", "walking", "working", "coding", "playing"];
+        if (adjArray.includes(params["student"])) {
+          let num = Math.floor(Math.random() * adjArray.length);
+          const objToJson = {
+            name: adjArray[num],
+            status: verbArray[num],
+            currentOccupation: "Leon",
+          };
+          res.end(JSON.stringify(objToJson));
+        } //student = leon
+        else if (params["student"] == "game") {
+          const objToJson = {
+            name: "tic-tac-toe",
+            status: "playing",
+            currentOccupation: "both",
+          };
+          res.end(JSON.stringify(objToJson));
+        } else {
+          const objToJson = {
+            name: "unknown",
+            status: "unknown",
+            currentOccupation: "unknown",
+          };
+          res.end(JSON.stringify(objToJson));
+        }
+      }
+      break;
+    case "/css/style.css":
+      fs.readFile("css/style.css", (err, data) => {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/tic-tac-toe/style.css":
+      fs.readFile("tic-tac-toe/style.css", (err, data) => {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/js/main.js":
+      createfile("js/main.js", "text/javascript");
+      break;
+    case "/tic-tac-toe/app.js":
+      createfile("tic-tac-toe/app.js", "text/javascript");
+      break;
+    default:
+      figlet("404!!", function (err, data) {
+        if (err) {
+          console.log("Something went wrong...");
+          console.dir(err);
+          return;
+        }
+        res.write(data);
+        res.end();
+      });
+  }
+});
+
+server.listen(8000);
---
+++ tic-tac-toe/app.js
@@ -0,0 +1,85 @@
+let CLASS_X = "x";
+let CLASS_CIRCLE = "circle";
+let currentTurn;
+const WINNININGBORD = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 5, 8],
+  [2, 4, 6],
+];
+const cellElement = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winningSmgElement = document.getElementById("winningMsg");
+const restartButton = document.getElementById("restartBtn");
+const winningSmg = document.querySelector("[data-winning-msg]");
+startGame();
+
+restartBtn.addEventListener("click", startGame);
+function startGame() {
+  currentTurn = false;
+  cellElement.forEach((cell) => {
+    cell.classList.remove(CLASS_X);
+    cell.classList.remove(CLASS_CIRCLE);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningSmgElement.classList.remove("show");
+}
+
+function handleClick(event) {
+  let cell = event.target;
+  let currentClass = currentTurn ? CLASS_CIRCLE : CLASS_X;
+  placeMarker(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningSmg.innerHTML = "Drawing";
+  } else {
+    winningSmg.innerHTML = `${currentTurn ? "O's" : "X's"} win's  `;
+  }
+  winningSmgElement.classList.add("show");
+}
+function placeMarker(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+function isDraw() {
+  return [...cellElement].every((cell) => {
+    return (
+      cell.classList.contains(CLASS_X) || cell.classList.contains(CLASS_CIRCLE)
+    );
+  });
+}
+function swapTurns() {
+  currentTurn = !currentTurn;
+}
+function setBoardHoverClass() {
+  board.classList.remove(CLASS_X);
+  board.classList.remove(CLASS_CIRCLE);
+  if (currentTurn) {
+    board.classList.add(CLASS_CIRCLE);
+  } else {
+    board.classList.add(CLASS_X);
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNININGBORD.some((comb) => {
+    return comb.every((idx) =>
+      cellElement[idx].classList.contains(currentClass)
+    );
+  });
+}
